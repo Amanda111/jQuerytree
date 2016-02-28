6ecd93e21d7050aaf4c66d643c7184c8dd532654
github <631551773@qq.com>
2016-02-28 18:19:57 +0800

    update

--- jquerytree.js
+++ jquerytree.js
@@ -1,23 +1,35 @@
-// $(document).ready(function(){
-// 	$("li ul").hide();
-// 	$("li:has(ul)").css("listStyleImage","url('hearta.PNG')");
-//   	$("ul").css("listStyleImage","url('heart.PNG')");
-// 	$("h5").click(function(){
-//     	$(this).siblings().toggle();
-//   	});
-// });
-
 $(document).ready(function(){
 	$("li").children().hide();
 	$("li:has(ul)").addClass("listimage");
-	$("li ul").addClass("sonimage");
+	$("li:not(:has(ul))").addClass("sonimage");
 	$("li").click(function(event){
-		$(this).children().toggle();
-		event.stopPropagation();
-		if ($(this).is(".listimage")) {
-			$(this).removeClass("listimage").addClass("listimagea");
-		}else if ($(this).is(".listimagea")) {
-			$(this).removeClass("listimagea").addClass("listimage");
+		if (this==event.target) {
+			if ($(this).is(".listimage")) {
+				$(this).children().slideDown("fast",function(){
+					$(this).parent().removeClass("listimage").addClass("listimagea");
+				});
+			}else if ($(this).is(".listimagea")) {
+				$(this).children().slideUp("fast",function(){
+				$(this).parent().removeClass("listimagea").addClass("listimage");
+				});
+			};
 		};
 	});
 });
+
+//initial
+
+// $(document).ready(function(){
+// 	$("li").children().hide();
+// 	$("li:has(ul)").addClass("listimage");
+// 	$("li ul").addClass("sonimage");
+// 	$("li").click(function(event){
+// 		$(this).children().toggle();
+// 		event.stopPropagation();
+// 		if ($(this).is(".listimage")) {
+// 			$(this).removeClass("listimage").addClass("listimagea");
+// 		}else if ($(this).is(".listimagea")) {
+// 			$(this).removeClass("listimagea").addClass("listimage");
+// 		};
+// 	});
+// });
